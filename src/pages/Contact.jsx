3ea66d323a52506dd6953097503a1b3aa3092dc6
Anup2414 <121458038+Anup2414@users.noreply.github.com>
2024-08-29 08:59:33 +0530

    About us
    added abous section and details

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,12 +5,14 @@ import ReviewSlider from "../components/Common/ReviewSlider"
 import ContactDetails from "../components/core/ContactUsPage/ContactDetails"
 import ContactForm from "../components/core/ContactUsPage/ContactForm"
 
+
 const Contact = () => {
   return (
     <div>
       <div className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row">
         {/* Contact Details */}
         <div className="lg:w-[40%]">
+          
           <ContactDetails />
         </div>
 
